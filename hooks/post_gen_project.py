8bb23fdd63ee93a91eb17a970d87842c47181a6c
Adelmo Filho <adelmo.aguiar.filho@gmail.com>
2020-03-17 21:57:13 -0300

    add Rpost hook

--- hooks/post_gen_project.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env R
-# -*- coding: utf-8 -*-
-
-InsPack <- function(pack) { if (!pack %in% installed.packages()) { print(paste("installing",pack)) install.packages(pack) } else print(paste(pack," already installed")) }
-
-InsPack("renv")
