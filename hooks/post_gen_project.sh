e686f3f75c4b97cdc217159bf175e7cd04c18639
Adelmo Filho <adelmo.aguiar.filho@gmail.com>
2020-03-17 23:07:16 -0300

    ajuste da licensa

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -36,7 +36,7 @@ define_license <- function(license) {
 
 verify_brefore_install("usethis")
 
-define_license({{cookiecutter.license}})
+define_license(paste({{cookiecutter.license}}))
 
   
 # Criação da virtualenv para R
